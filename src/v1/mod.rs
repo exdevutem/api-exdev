259972972ca562dae7245e948b0922bee52edb9f
Rafael Morales Venegas <rmorales@utem.cl>
2023-06-13 16:09:35 -0400

    small route refactor

--- src/v1/mod.rs
+++ src/v1/mod.rs
@@ -5,12 +5,5 @@ pub mod res;
 pub mod schemas;
 
 pub fn routes() -> actix_web::Scope {
-    web::scope("/v1").service(
-        web::scope("/members")
-            .service(res::club_members::get_club_members)
-            .service(res::club_members::get_single_member)
-            .service(res::club_members::add_club_member)
-            .service(res::club_members::update_club_member)
-            .service(res::club_members::delete_member),
-    )
+    web::scope("/v1").service(res::add_member_routes())
 }
--- src/v1/res/mod.rs
+++ src/v1/res/mod.rs
@@ -1,1 +1,10 @@
 pub mod club_members;
+
+pub fn add_member_routes() -> actix_web::Scope {
+    actix_web::web::scope("/members")
+        .service(club_members::get_club_members)
+        .service(club_members::get_single_member)
+        .service(club_members::add_club_member)
+        .service(club_members::update_club_member)
+        .service(club_members::delete_member)
+}
