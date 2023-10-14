2bf0119bee2df6f542678671e2f23b53edee833f
Rafael Morales <rmorales@utem.cl>
2023-10-14 16:19:56 -0300

    Docs for `/src/v1/schemas/*.rs`

--- src/v1/schemas/mod.rs
+++ src/v1/schemas/mod.rs
@@ -1,2 +1,7 @@
+//! Módulo de inputs esperados de parte del usuario.
+//!
+//! Este módulo busca estandarizar los inputs esperados al momento de crear o actualizar recursos
+//! por parte de usuarios de la API.
+
 pub mod auth;
 pub mod club_member;
