7e8e557f2e208be6296deef2fd620308f711d15a
Rafael Morales <rmorales@utem.cl>
2023-12-24 15:17:10 -0300

    Por fin una wea que si funciona

--- indexer/src/main.rs
+++ indexer/src/main.rs
@@ -2,32 +2,6 @@
 
 use clap::Parser;
 
-use pdf::content::Op;
-
-fn op_to_string(op: &Op) -> String {
-    match op {
-        Op::TextDraw { text } => {
-            format!("{datos} ", datos = text.to_string_lossy())
-        }
-        Op::TextDrawAdjusted { array } => {
-            let text = array.iter().fold(String::new(), |mut acc, txt| {
-                use pdf::content::TextDrawAdjusted::Text;
-
-                if let Text(texto) = txt {
-                    let texto = texto.to_string_lossy();
-
-                    acc.push_str(texto.as_str());
-                }
-
-                acc
-            });
-
-            text
-        }
-        _ => String::new(),
-    }
-}
-
 #[derive(Parser, Debug)]
 struct Args {
     path: std::path::PathBuf,
@@ -36,27 +10,16 @@ struct Args {
 fn main() -> anyhow::Result<()> {
     let args = Args::parse();
 
-    let file = pdf::file::FileOptions::cached().open(args.path)?;
-    let resolver = file.resolver();
+    let file = std::fs::read(args.path).unwrap();
 
-    let text = file
-        .pages()
-        .filter_map(Result::ok)
-        .filter_map(|page| page.contents.clone())
-        .map(|content| {
-            content
-                .operations(&resolver)
-                .unwrap()
-                .iter()
-                .map(|e| -> String { op_to_string(e) })
-                .fold(String::new(), |mut acc, s| {
-                    acc.push_str(s.as_str());
-                    acc
-                })
-        })
-        .collect::<String>();
+    let file = poppler::Document::from_data(file.as_slice(), None).unwrap();
 
-    println!("{text}");
+    for index in 0..file.n_pages() {
+        println!(
+            "{out}",
+            out = file.page(index).unwrap().text().unwrap().to_string()
+        );
+    }
 
     Ok(())
 }
