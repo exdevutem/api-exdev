04a09b3dc33d1e4bf0078587e51633010fa0e64c
Rafael Morales <rmorales@utem.cl>
2023-12-24 11:55:32 -0300

    ejemplos para trabajar, aun no funca

--- indexer/src/main.rs
+++ indexer/src/main.rs
@@ -1,30 +1,20 @@
 //! Indexador de documentos para plataforma estudiantil, utilizando la estrategia TF-IDF
 
 use clap::Parser;
-use std::ops::Deref;
 
 use pdf::content::Op;
 
 fn op_to_string(op: &Op) -> String {
     match op {
         Op::TextDraw { text } => {
-            let datos = text
-                .as_bytes()
-                .iter()
-                .map(|&c| c as char)
-                .collect::<String>();
-
-            format!("{datos} ")
+            format!("{datos} ", datos = text.to_string_lossy())
         }
         Op::TextDrawAdjusted { array } => {
             let text = array.iter().fold(String::new(), |mut acc, txt| {
                 use pdf::content::TextDrawAdjusted::Text;
+
                 if let Text(texto) = txt {
-                    let texto = texto
-                        .as_bytes()
-                        .iter()
-                        .map(|&c| c as char)
-                        .collect::<String>();
+                    let texto = texto.to_string_lossy();
 
                     acc.push_str(texto.as_str());
                 }
@@ -46,27 +36,27 @@ struct Args {
 fn main() -> anyhow::Result<()> {
     let args = Args::parse();
 
-    let pdf = pdf::file::FileOptions::cached().open(args.path)?;
+    let file = pdf::file::FileOptions::cached().open(args.path)?;
+    let resolver = file.resolver();
 
-    for page in pdf.pages().filter_map(Result::ok) {
-        let page = page.deref();
+    let text = file
+        .pages()
+        .filter_map(Result::ok)
+        .filter_map(|page| page.contents.clone())
+        .map(|content| {
+            content
+                .operations(&resolver)
+                .unwrap()
+                .iter()
+                .map(|e| -> String { op_to_string(e) })
+                .fold(String::new(), |mut acc, s| {
+                    acc.push_str(s.as_str());
+                    acc
+                })
+        })
+        .collect::<String>();
 
-        let content = match &page.contents {
-            Some(c) => c,
-            None => continue,
-        };
-
-        let parrafo = content
-            .operations(&pdf)?
-            .iter()
-            .map(|e| -> String { op_to_string(e) })
-            .fold(String::new(), |mut acc, s| {
-                acc.push_str(s.as_str());
-                acc
-            });
-
-        println!("{parrafo}");
-    }
+    println!("{text}");
 
     Ok(())
 }
